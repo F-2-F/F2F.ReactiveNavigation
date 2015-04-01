40e5ef53970f8c61449b170ef173614647fd3f4d
codedevote <ff@ff-solutions.de>
2015-04-01 20:34:13 +0200

    Implement a ReactiveItemsViewModel #4

---
+++ src/F2F.ReactiveNavigation/PropertyName.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Linq.Expressions;
+
+using dbc = System.Diagnostics.Contracts;
+
+namespace F2F.ReactiveNavigation
+{
+	/// <summary>
+	/// Supporting functions for property projection lambdas.
+	/// </summary>
+	public static class PropertyName
+	{
+		/// <summary>
+		/// Extracts the property name from a property projection lambda.
+		/// </summary>
+		/// <typeparam name="TProperty">Property type</typeparam>
+		/// <param name="projection">Property projection function</param>
+		/// <returns>Property name</returns>
+		/// /// <example>
+		/// string propName = PropertyName.From(() => someObj.SomeProperty);
+		/// Assert.AreEqual(propName, "SomeProperty");
+		/// </example>
+		public static string Of<TProperty>(Expression<Func<TProperty>> projection)
+		{
+			#region contract
+
+			dbc.Contract.Requires<ArgumentNullException>(projection != null,
+							  "Projection cannot be null.");
+
+			dbc.Contract.Requires<ArgumentException>(projection.Body is MemberExpression,
+							  "Projection must be a member expression.");
+
+			// can't test for property expression in portable lib (at least I don't know how)
+			// MemberType is not defined
+			//dbc.Contract.Requires<ArgumentException>((projection.Body as MemberExpression).Member.MemberType == System.Reflection.MemberTypes.Property,
+			//				  "Projection must select a property.");
+
+			#endregion contract
+
+			var body = projection.Body as MemberExpression;
+
+			return body.Member.Name;
+		}
+
+		/// <summary>
+		/// Extracts the property name from a property projection lambda.
+		/// </summary>
+		/// <typeparam name="TProperty">Property type</typeparam>
+		/// <param name="projection">Property projection function</param>
+		/// <returns>Property name</returns>
+		/// /// <example>
+		/// string propName = PropertyName.From(() => someObj.SomeProperty);
+		/// Assert.AreEqual(propName, "SomeProperty");
+		/// </example>
+		public static string Of<TViewModel, TProperty>(Expression<Func<TViewModel, TProperty>> projection)
+		{
+			#region contract
+
+			dbc.Contract.Requires<ArgumentNullException>(projection != null,
+							  "Projection cannot be null.");
+
+			dbc.Contract.Requires<ArgumentException>(projection.Body is MemberExpression,
+							  "Projection must be a member expression.");
+
+			// can't test for property expression in portable lib (at least I don't know how)
+			// MemberType is not defined
+			//dbc.Contract.Requires<ArgumentException>((projection.Body as MemberExpression).Member.MemberType == System.Reflection.MemberTypes.Property,
+			//				  "Projection must select a property.");
+
+			#endregion contract
+
+			var body = projection.Body as MemberExpression;
+
+			return body.Member.Name;
+		}
+	}
+}
---
+++ src/F2F.ReactiveNavigation/ViewModel/ReactiveValidatedViewModel.cs
@@ -0,0 +1,101 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace F2F.ReactiveNavigation.ViewModel
+{
+	
+	public class ReactiveValidatedViewModel : ReactiveViewModel, INotifyDataErrorInfo
+	{
+		/// <summary>
+		/// "Empty" validator whose single purpose is to return a positive validation result.
+		/// </summary>
+		private class AlwaysValidValidator : AbstractValidator<object>
+		{
+		}
+
+		/// <summary>
+		/// ValidationResult that always returns success
+		/// </summary>
+		private class ValidationSuccess : ValidationResult
+		{
+			public ValidationSuccess()
+			{
+			}
+
+			public ValidationSuccess(System.Collections.Generic.IEnumerable<ValidationFailure> failures)
+				: base()
+			{
+			}
+
+			public override bool IsValid
+			{
+				get { return true; }
+			}
+		}
+
+
+		public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+		private ObservableAsPropertyHelper<bool> _hasErrors;
+		private ValidationResult _validationResults = new ValidationSuccess();
+
+		protected override void Init()
+		{
+			
+		}
+
+		public System.Collections.IEnumerable GetErrors(string propertyName)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool HasErrors
+		{
+			get { return _hasErrors.Value; }
+		}
+
+		protected bool Validate()
+		{
+			var previousResults = _validationResults;
+			var validator = ProvideValidator();
+
+			_validationResults = validator.Validate(this);
+			
+			// raise errors changed event for union set of previous and current results
+			// so a change notification is raised for each affected property
+			var propertiesInError = 
+				previousResults
+					.GetPropertiesInError()
+					.Union(_validationResults.GetPropertiesInError());
+
+			foreach(var property in propertiesInError)
+				RaiseErrorsChanged(property);
+
+			return _validationResults.IsValid;
+		}
+
+		protected virtual void RaiseErrorsChanged(string propertyName)
+		{
+			EventHandler<DataErrorsChangedEventArgs> handler = ErrorsChanged;
+			if (handler != null)
+				handler(this, new DataErrorsChangedEventArgs(propertyName));
+		}
+
+		protected virtual void RaiseErrorsChanged<TProperty>(Expression<Func<TProperty>> projection)
+		{
+			RaiseErrorsChanged(PropertyName.Of(projection));
+		}
+
+		protected virtual IValidator ProvideValidator()
+		{
+			return new AlwaysValidValidator();
+		}
+		
+	}
+}
