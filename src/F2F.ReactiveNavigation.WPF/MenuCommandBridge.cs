16795e9abde6bcee66ef3a56f357cb18269b64c0
jaimegv <jaimegv@gmail.com>
2015-12-18 16:23:32 +0100

    Removed unused and duplicated using statements

--- src/F2F.ReactiveNavigation.WPF/MenuCommandBridge.cs
+++ src/F2F.ReactiveNavigation.WPF/MenuCommandBridge.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
-using System.Reactive;
-using System.Windows.Controls;
 using ReactiveUI;
 
 namespace F2F.ReactiveNavigation.WPF
