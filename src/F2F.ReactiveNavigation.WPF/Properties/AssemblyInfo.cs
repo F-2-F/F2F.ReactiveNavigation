c89b3477160c9012abb10cb02614fa2e85e25959
Florian Fordermaier <ff@ff-solutions.de>
2016-01-12 09:29:28 +0100

    Bump version to 2.0.4-beta

--- src/F2F.ReactiveNavigation.WPF/Properties/AssemblyInfo.cs
+++ src/F2F.ReactiveNavigation.WPF/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("F2F.ReactiveNavigation.WPF")]
 [assembly: AssemblyProductAttribute("F2F.ReactiveNavigation")]
 [assembly: AssemblyDescriptionAttribute("Region-based navigation on top of ReactiveUI and some extra goodies")]
-[assembly: AssemblyVersionAttribute("2.0.3")]
-[assembly: AssemblyFileVersionAttribute("2.0.3")]
+[assembly: AssemblyVersionAttribute("2.0.4")]
+[assembly: AssemblyFileVersionAttribute("2.0.4")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "2.0.3";
+        internal const string Version = "2.0.4";
     }
 }
