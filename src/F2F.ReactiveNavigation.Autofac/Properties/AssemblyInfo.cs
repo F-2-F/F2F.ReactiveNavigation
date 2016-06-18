7d2cc933fd2b6dddf8224c84ef98daa0b81a9bd9
codedevote <ff@ff-solutions.de>
2016-06-18 20:08:39 +0200

    Bump version to 2.1.0-beta

--- src/F2F.ReactiveNavigation.Autofac/Properties/AssemblyInfo.cs
+++ src/F2F.ReactiveNavigation.Autofac/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("F2F.ReactiveNavigation.Autofac")]
 [assembly: AssemblyProductAttribute("F2F.ReactiveNavigation")]
 [assembly: AssemblyDescriptionAttribute("Region-based navigation on top of ReactiveUI and some extra goodies")]
-[assembly: AssemblyVersionAttribute("2.0.4")]
-[assembly: AssemblyFileVersionAttribute("2.0.4")]
+[assembly: AssemblyVersionAttribute("2.1.0")]
+[assembly: AssemblyFileVersionAttribute("2.1.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "2.0.4";
+        internal const string Version = "2.1.0";
     }
 }
