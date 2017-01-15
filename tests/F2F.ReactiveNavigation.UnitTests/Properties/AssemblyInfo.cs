9517969df45a0467ee6b904457fbd1921b9f7ebc
Florian Fordermaier <ff@ff-solutions.de>
2017-01-15 09:33:15 +0100

    Relax versions of dependencies to follow semver #62
    Fixed flakey tests by disabling parallelization of tests for xunit2.

--- tests/F2F.ReactiveNavigation.UnitTests/Properties/AssemblyInfo.cs
+++ tests/F2F.ReactiveNavigation.UnitTests/Properties/AssemblyInfo.cs
@@ -29,3 +29,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
+
+[assembly: Xunit.CollectionBehavior(DisableTestParallelization =true, MaxParallelThreads =1)]
