d5fb59fa18c57c465a55955f1c504bc3cea0b38b
Florian Fordermaier <ff@ff-solutions.de>
2018-01-17 15:41:33 +0100

    Bumped version 4.0.1

--- src/F2F.ReactiveNavigation.WPF.Sample/Properties/AssemblyInfo.cs
+++ src/F2F.ReactiveNavigation.WPF.Sample/Properties/AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("F2F.ReactiveNavigation.WPF.Sample")]
 [assembly: AssemblyProductAttribute("F2F.ReactiveNavigation")]
 [assembly: AssemblyDescriptionAttribute("Region-based navigation on top of ReactiveUI and some extra goodies")]
-[assembly: AssemblyVersionAttribute("4.0.0")]
-[assembly: AssemblyFileVersionAttribute("4.0.0")]
+[assembly: AssemblyVersionAttribute("4.0.1")]
+[assembly: AssemblyFileVersionAttribute("4.0.1")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "F2F.ReactiveNavigation.WPF.Sample";
         internal const System.String AssemblyProduct = "F2F.ReactiveNavigation";
         internal const System.String AssemblyDescription = "Region-based navigation on top of ReactiveUI and some extra goodies";
-        internal const System.String AssemblyVersion = "4.0.0";
-        internal const System.String AssemblyFileVersion = "4.0.0";
+        internal const System.String AssemblyVersion = "4.0.1";
+        internal const System.String AssemblyFileVersion = "4.0.1";
     }
 }
