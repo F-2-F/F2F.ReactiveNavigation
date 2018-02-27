f60974bd7cbb8a8300b62febb137278bffc02789
Florian Fordermaier <ff@ff-solutions.de>
2018-02-27 17:26:09 +0100

    Bump version

--- src/F2F.ReactiveNavigation/Properties/AssemblyInfo.cs
+++ src/F2F.ReactiveNavigation/Properties/AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Reflection;
 [assembly: AssemblyTitleAttribute("F2F.ReactiveNavigation")]
 [assembly: AssemblyProductAttribute("F2F.ReactiveNavigation")]
 [assembly: AssemblyDescriptionAttribute("Region-based navigation on top of ReactiveUI and some extra goodies")]
-[assembly: AssemblyVersionAttribute("4.0.5")]
-[assembly: AssemblyFileVersionAttribute("4.0.5")]
+[assembly: AssemblyVersionAttribute("4.0.6")]
+[assembly: AssemblyFileVersionAttribute("4.0.6")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "F2F.ReactiveNavigation";
         internal const System.String AssemblyProduct = "F2F.ReactiveNavigation";
         internal const System.String AssemblyDescription = "Region-based navigation on top of ReactiveUI and some extra goodies";
-        internal const System.String AssemblyVersion = "4.0.5";
-        internal const System.String AssemblyFileVersion = "4.0.5";
+        internal const System.String AssemblyVersion = "4.0.6";
+        internal const System.String AssemblyFileVersion = "4.0.6";
     }
 }
